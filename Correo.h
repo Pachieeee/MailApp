d44283ced45d6844b3eba8fba79d75d089a97311
Pache <sebaspacheco2003@outlook.es>
2023-10-02 11:49:32 -0500

    Olvidé corregir los include

--- Correo.h
+++ Correo.h
@@ -1,9 +1,9 @@
 #pragma once
-#include "CPrincipal.h"
-#include "CSpam.h"
-#include "CEnviado.h"
-#include "CPapelera.h"
-#include "CFijado.h"
+#include "Inbox/CPrincipal.h"
+#include "Inbox/CSpam.h"
+#include "Inbox/CEnviado.h"
+#include "Inbox/CPapelera.h"
+#include "Inbox/CFijado.h"
 #include "Busqueda.h"
 class Correo {
 private:
